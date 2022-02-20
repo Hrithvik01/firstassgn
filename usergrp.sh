af2db3adcfdea28f05a2a6e4d1c517a642045c81
Hrithvik Atluri <hrithvikatluri@gmail.com>
2022-02-20 16:05:01 +0530

    this is in b2

--- usergrp.sh
+++ usergrp.sh
@@ -1,13 +1,3 @@
-addsomeone(){
-echo 'input here'
-read x
-if [ $x == '1' ]
-then
-add_user
-else [ $x == 2 ]
-add_group
-fi
-}
 add_user(){
     echo 'input new user name here'
     read x
@@ -28,6 +18,41 @@ add_group(){
     echo "This group is successfully added to $USER"
     groups $USER
 }
-addsomeone
+sys_info(){
+echo  " HOSTNAME INFORMATION "
+hostnamectl
+echo ""
+echo  "FILE SYSTEM DISK SPACE USAGE "
+df -h
+echo ""
+echo  "FREE AND USED MEMORY"
+free
+echo ""
+echo  "SYSTEM UPTIME AND LOAD"
+uptime
+echo ""
+echo  "CURRENTLY LOGGED-IN USERS "
+who
+echo ""
+echo  "TOP 5 MEMORY-CONSUMING PROCESSES "
+ps -eo  %mem,%cpu,comm --sort=-%mem | head -n 6
+echo ""
+echo "Done"
+}
+addsomeone(){
+echo 'click 1 for adding a user, 2 for adding a group, 3 to check system info'
+echo 'input here'
+read x
+if [ $x == '1' ]
+then
+add_user
+elif [ $x == 2 ]
+then
+add_group
+else [ $x == 3 ]
+sys_info
+fi
+}
 
+addsomeone
 
